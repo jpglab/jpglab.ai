88b87f74de93947eff50a66ebc53a1dcbdb1f843
kevinschaich <schaich.kevin@gmail.com>
2025-09-14 15:43:45 -0400

    opengraph card

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,28 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-screen overflow-hidden w-full m-0 p-0 bg-simulation-bg">
-      <body className={`${jersey10.variable} flex flex-col overflow-hidden w-full h-screen relative m-0 p-0 border-none bg-simulation-bg`}>
-        {children}
-      </body>
-    </html>
+      <html lang="en" className="h-screen overflow-hidden w-full m-0 p-0 bg-simulation-bg">
+          <head>
+              <meta charSet="utf-8" />
+              <link rel="icon" type="image/svg+xml" href="/jpglab.png" />
+              <meta name="viewport" content="width=device-width" />
+              <title>jpglab</title>
+              <meta
+                  name="description"
+                  content="we're building a protocol for large language models to directly interface with image sensors."
+              />
+              <meta property="og:title" content="jpglab" />
+              <meta property="og:description" content="we're building a protocol for large language models to directly interface with image sensors." />
+              <meta property="og:image" content="/og.png" />
+              <meta property="og:type" content="website" />
+              <meta property="og:url" content="https://jpglab.ai" />
+              <meta name="twitter:card" content="summary_large_image" />
+          </head>
+          <body
+              className={`${jersey10.variable} flex flex-col overflow-hidden w-full h-screen relative m-0 p-0 border-none bg-simulation-bg`}
+          >
+              {children}
+          </body>
+      </html>
   )
 }
